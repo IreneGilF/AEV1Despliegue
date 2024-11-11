f6046940eb928f820661c2064d0c24ef3e60eb1b
ireneG <irgifu@alumnatflorida.es>
2024-11-11 18:47:05 +0100

    Modifico el php para añadir el ganado.css

--- ganado.php
+++ ganado.php
@@ -1,7 +1,17 @@
-<?php
-session_start();
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>¡Has ganado!</title>
+    <link rel="stylesheet" href="ganado.css">
+</head>
+<body>
+    <?php
+    session_start();
     echo "<h1>¡Has ganado!</h1>";
     echo "<p>La palabra era: " . $_SESSION['palabra'] . "</p>";
     echo '<a href="index.php">Juega de nuevo</a>';
     session_destroy();
-?>
+    ?>
+</body>
+</html>
