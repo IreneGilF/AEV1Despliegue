8efa7d96bbe52c6a7fa444589d56e1bd1567aa64
ireneG <irgifu@alumnatflorida.es>
2024-11-11 18:55:30 +0100

    Modifico perdido.php para añadirle el css

--- perdido.php
+++ perdido.php
@@ -1,7 +1,17 @@
-<?php
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>¡Has perdido!</title>
+    <link rel="stylesheet" href="perdido.css">
+</head>
+<body>
+    <?php
     session_start();
-    echo "<h1>¡Oh no! Has perdido</h1>";
+    echo "<h1>¡Oh no! Has perdido :( </h1>";
     echo "<p>La palabra era: " . $_SESSION['palabra'] . "</p>";
     echo '<a href="index.php">Jugar de nuevo</a>';
     session_destroy();
-?>
+    ?>
+</body>
+</html>
